0edd7126982088ce8a79569c2473b8173731a73b
Didier Zuñiga <didier35@hotmail.com>
2019-08-08 18:57:32 -0500

    Updates

---
+++ precache-manifest.a8d3e43e9ebb15fd89cc51abce3141dc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac24035a92742e9249c63fcf6cbfc069",
+    "url": "/mockup/index.html"
+  },
+  {
+    "revision": "890be22b5a6e16210865",
+    "url": "/mockup/static/css/main.cca4fa21.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/mockup/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "890be22b5a6e16210865",
+    "url": "/mockup/static/js/main.752d2427.chunk.js"
+  },
+  {
+    "revision": "6f2ef31a654e12acddb8",
+    "url": "/mockup/static/js/runtime~main.43ccd313.js"
+  },
+  {
+    "revision": "0682bb20b8c18976eaee166152e8a8e6",
+    "url": "/mockup/static/media/icomoon.0682bb20.eot"
+  },
+  {
+    "revision": "bf0acd007f16962c500473e3b21d7a14",
+    "url": "/mockup/static/media/icomoon.bf0acd00.svg"
+  },
+  {
+    "revision": "c78b662f416c4b627028ce5a83ae1d90",
+    "url": "/mockup/static/media/icomoon.c78b662f.ttf"
+  },
+  {
+    "revision": "fc188e2ae3a4f1f1bdbf38379b583960",
+    "url": "/mockup/static/media/icomoon.fc188e2a.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mockup/static/media/logo.5d5d9eef.svg"
+  }
+]);
